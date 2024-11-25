6d84fbc90ac8af4c30527840bdd43a244ac1ba1f
kazaraal <araalkaz@gmail.com>
2024-11-25 08:47:10 +0000

    The beginnings of a terraform-aws project to build a wordpress site with database backend

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform{
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+      }
+    }
+    required_version = "= 1.9.8"
+}
+
+# Create a vpc
+
+# Create a private subnet
+
+# Create a public subnet
+
+# Create a IGW
+
+# Create a public route table for the public subnet
+
+# Associate the public route table to the public subnet
+
